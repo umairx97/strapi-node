3c827fa0b95da92d0a2c8f83b83fa4b56730ce90
Umair Bajwa <umairx97>
2021-04-30 22:18:11 +0500

    feat: setup mongodb

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db')
-      },
-      options: {
-        useNullAsDefault: true
+const { MONGO_URI } = process.env || {}
+module.exports = ({ env }) => {
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings: {
+          uri: env('DATABASE_URI', MONGO_URI || 'mongodb://localhost:27017/strapi-node'),
+          srv: env.bool('DATABASE_SRV', true)
+        }
       }
     }
   }
-})
+}
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '407a2ad3b6e1830a885867adc4244846')
