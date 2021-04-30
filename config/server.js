34b2ac72094207a9e72466e811fdc55e2279a72c
Umair Bajwa <umairx97>
2021-04-30 21:23:11 +0500

    feat: setup strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '407a2ad3b6e1830a885867adc4244846')
+    }
+  }
+})
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '004a34df-2b77-4fad-a7d5-be67de05d200'
+}
